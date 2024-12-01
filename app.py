a93816367c1457c2968a50859a95b251ae294a92
cccv <sergey.s.saveliev@yandex.ru>
2024-12-01 17:03:34 +0700

    333

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+'''
+Не устанавливай расширение code runner
+pip uninstall -y PyQt6 PyQt6-Qt6 PyQt6-sip PyQt6-WebEngine PyQt6-WebEngine-Qt6
+pip uninstall -y PySide6 PySide6-Addons PySide6-Essential shiboken6
+pip install PyQt6==6.7.1 PyQt6-Qt6==6.7.1 PyQt6-WebEngine-Qt6==6.7.1 PyQt6-WebEngine
+pip install PySide6-Essentials==6.7.1 PySide6==6.7.1 PySide6-Addons==6.7.1 shiboken6==6.7.1
+'''
 import sys, os, serial, glob, datetime, logging, logging.handlers
 from configparser import ConfigParser
 from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox, 
